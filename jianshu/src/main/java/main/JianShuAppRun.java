a39a1697dcd981a9002b12ef3c6b6daa9f91c9d5
linqi <linq@bsfit.com.cn>
2016-10-09 21:28:39 +0800

    爬取两个千赞专题

---
+++ jianshu/src/main/java/main/JianShuAppRun.java
@@ -0,0 +1,26 @@
+package main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import utils.PropertiesUtil;
+
+public class JianShuAppRun {
+	private static Logger lg = LoggerFactory.getLogger(JianShuAppRun.class);
+	private static String collectionIds = PropertiesUtil.getValueByKey("config/spider.properties", "collectionIds");
+	public static void main(String[] args) {
+		String[] ids = collectionIds.split(",");
+		if(ids.length==0){
+			lg.info("Collection id not set in properties file.");
+			return;
+		}else{
+			lg.info("Collection id:{}",collectionIds);
+		}
+		for(int i=0;i<ids.length;i++){
+			MultiThreadRun thread=new MultiThreadRun(Integer.valueOf(ids[i]));
+			thread.setName("Thread-"+ids[i]);
+			thread.start();
+		}
+	}
+
+}
