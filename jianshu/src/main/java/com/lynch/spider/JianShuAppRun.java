8fb567494f491560b7d7852638054e18ed01c0b2
fire <lynch0571@gmail.com>
2017-09-05 15:44:24 +0800

    request after running

--- jianshu/src/main/java/com/lynch/spider/JianShuAppRun.java
+++ jianshu/src/main/java/com/lynch/spider/JianShuAppRun.java
@@ -1,5 +1,9 @@
 package com.lynch.spider;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,14 @@ public class JianShuAppRun {
 
     public static void main(String[] args) {
         SpringApplication.run(JianShuAppRun.class, args);
+        System.out.println("程序已启动");
+        try {
+			Document doc=Jsoup.connect("http://localhost:8080/a/crawl/db").get();
+			System.out.println(doc);
+		} catch (IOException e) {
+			System.err.println("调用请求出错");
+			e.printStackTrace();
+		}
     }
 
 }
